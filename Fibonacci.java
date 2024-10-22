a3896853f2cd3eb9be4281c74783d8ded12f80a7
nikola_devic <ndevic@oakland.edu>
2024-10-22 14:06:10 -0400

    added jar

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,28 +1,32 @@
 public class Fibonacci {
     public static void main(String[] args) {
-        if (args.length < 1)
-        {
+        // If no number arguement
+        if (args.length < 1) {
             System.out.println("Please enter a valid integer.");
             return;
         }
-        else if (args.length > 1)
-        {
+        // If too many arguements
+        else if (args.length > 1) {
             System.out.println("Too many arguements");
             return;
         }
 
+        // Turn string into valid int
         int num = Integer.parseInt(args[0]);
         System.out.println(fibonacci(num));
     }
     
     public static int fibonacci(int num) {
+        // Base case for 0
         if (num == 0) {
             return 0;
         }
+        // Base case for 1
         if (num == 1) {
             return 1;
         }
 
+        // Recursion
         return fibonacci(num - 1) + fibonacci(num - 2);
     }
 }
